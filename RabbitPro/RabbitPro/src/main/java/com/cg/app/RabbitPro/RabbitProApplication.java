e1951abb8aed2eda8c5ff958d3e353a603569920
Kadlag <punam.kadlag@capgemini.com>
2019-01-30 14:30:47 +0530

    add simple hello-World

---
+++ RabbitPro/RabbitPro/src/main/java/com/cg/app/RabbitPro/RabbitProApplication.java
@@ -0,0 +1,14 @@
+package com.cg.app.RabbitPro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class RabbitProApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RabbitProApplication.class, args);
+	}
+
+}
---
+++ RabbitReceiver/RabbitReceiver/src/main/java/com/cg/app/RabbitReceiver/messaging/Receiver.java
@@ -0,0 +1,21 @@
+package com.cg.app.RabbitReceiver.messaging;
+
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Receiver {
+
+	@Bean
+	public Queue queue() {
+		return new Queue("CustomerQ", false);
+	}
+
+	@RabbitListener(queues = "CustomerQ")
+	public void processMessage(String message) {
+		System.out.println(message + "Received");
+	}
+
+}
